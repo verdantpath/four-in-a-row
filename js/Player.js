c203ae7688c4a49bfacca934f0d63a6ee83d8917
verdantpath <m_dittmann@yahoo.com>
2018-12-04 21:08:44 -0800

    update Player and Token classes, add constructors

--- js/Player.js
+++ js/Player.js
@@ -1,3 +1,9 @@
 class Player {
-
+  constructor(name, id, color, active = false, tokens) {
+    this.name = name;
+    this.id = id;
+    this.color = color;
+    this.active = active;
+    this.tokens = tokens[];
+  }
 }
--- js/Token.js
+++ js/Token.js
@@ -1,3 +1,7 @@
 class Token {
-
+  constructor(this, owner) {
+    this.owner = owner;
+    this.id = `token-${index}-${owner.id}`;
+    this.dropped = false;
+  }
 }
