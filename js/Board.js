5c6868411225ac149d4e3843c2b8e21f562c22f7
verdantpath <m_dittmann@yahoo.com>
2018-12-12 18:12:53 -0800

    update js, createSpaces method

--- js/Board.js
+++ js/Board.js
@@ -4,8 +4,23 @@ class Board {
     this.columns = 7;
     this.spaces = createSpaces();
   }
-
+/**
+ * Generates 2D array of spaces.
+ * @return {Array} And array of space objects
+ */
   createSpaces() {
+    const spaces = [];
 
+    for (let x = 0; x < this.columns; x++) {
+      const column = [];
+
+      for (let y = 0; y < this.rows; y++) {
+        const space = new Space(x, y);
+        column.push(space);
+      }
+
+      spaces.push(column);
   }
+
+  return spaces;
 }
