15f51fa346777eb2df7e3759f7e1761f011664f5
verdantpath <m_dittmann@yahoo.com>
2018-12-13 20:30:44 -0800

    update Game class, add constructor and creator method for players

--- js/Game.js
+++ js/Game.js
@@ -1,3 +1,14 @@
 class Game {
-
+  constructor() {
+    this.board = new Board;
+    this.players = this.createPlayers();
+    this.ready = false;
+  }
+/**
+ * creates two player objects
+ * @return {Array} An array of two Player objects
+ */
+  createPlayers {
+    
+  }
 }
