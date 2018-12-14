ca46621c45a3028b39f2e9f9e2ff57d5805939ea
verdantpath <m_dittmann@yahoo.com>
2018-12-13 21:31:12 -0800

    update creator method

--- js/Game.js
+++ js/Game.js
@@ -1,14 +1,17 @@
 class Game {
   constructor() {
-    this.board = new Board;
+    this.board = new Board();
     this.players = this.createPlayers();
     this.ready = false;
   }
+
 /**
  * creates two player objects
  * @return {Array} An array of two Player objects
  */
   createPlayers {
-    
+    const players = [new Player('Player 1', 1, '#e15258', true),
+                      new Player('Player 2', 2, '#e59a13')];
+    return players;
   }
 }
