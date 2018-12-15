99c602415abe76b3ecc7c3e471ede730774e1efc
verdantpath <m_dittmann@yahoo.com>
2018-12-15 07:39:33 -0800

    refactor

--- js/Game.js
+++ js/Game.js
@@ -9,11 +9,13 @@ class Game {
  * creates two player objects
  * @return {Array} An array of two Player objects
  */
-  createPlayers {
+  createPlayers() {
     const players = [new Player('Player 1', 1, '#e15258', true),
-                      new Player('Player 2', 2, '#e59a13')];
+                    new Player('Player 2', 2, '#e59a13')];
     return players;
   }
-
-  startGame {}
+/**
+ * Gets game ready for play
+ */
+  startGame() {}
 }
