a2aca2bfae04026971c6f1dfe29cda3977061c3a
verdantpath <m_dittmann@yahoo.com>
2018-12-15 08:59:34 -0800

    render method for SVG spaces

--- js/Space.js
+++ js/Space.js
@@ -4,5 +4,18 @@ class Space {
     this.y = y;
     this.id = `space-${x}-${y}`;
     this.token = null;
+    this.diameter = 76;
+    this.radius = this.diameter / 2;
+  }
+
+  drawSVGSpace() {
+    constSVGSpace = document.createElementNS("http://www.w3.org/2000/svg", "circle");
+    svgSpace.setAttributesNS(null, "id", this.id);
+    svgspace.setAttributesNs(null, "cx", (this.x * this.diameter) + this.radius);
+    svgSpace.setAttributesNS(null, "cy", (this.y * this.diameter) + this.radius);
+    svgSpace.setAttributesNS(null, "r", this.radius - 8);
+    svgSpace.setAttributesNS(null, "fill", "black");
+    svgSpace.setAttributesNS(null, "stroke", "none");
+    document.getElementById("mask").appendChild(svgSpace);
   }
 }
