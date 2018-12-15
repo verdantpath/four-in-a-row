be712048d942abeee632f26738d901831ad2146e
verdantpath <m_dittmann@yahoo.com>
2018-12-14 20:36:48 -0800

    update app.js

--- js/app.js
+++ js/app.js
@@ -1,1 +1,11 @@
-new Game();
+const game = new Game();
+
+/**
+ * Listens for click on `#begin-game` and calls startGame() on game object
+ */
+const beginGame = document.getElementById('begin-game');
+beginGame.addEventListener('click', function() {
+  this.startGame();
+  this.style.display = 'none';
+  document.getElementById('play-area').style.opacity = '1';
+});
