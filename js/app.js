550dded20fdcfa5ee9aa084f3533ac2943fce538
verdantpath <m_dittmann@yahoo.com>
2018-12-15 07:40:07 -0800

    refactor

--- js/app.js
+++ js/app.js
@@ -3,9 +3,8 @@ const game = new Game();
 /**
  * Listens for click on `#begin-game` and calls startGame() on game object
  */
-const beginGame = document.getElementById('begin-game');
-beginGame.addEventListener('click', function() {
-  this.startGame();
+document.getElementById('begin-game').addEventListener('click', function() {
+  game.startGame();
   this.style.display = 'none';
   document.getElementById('play-area').style.opacity = '1';
 });
