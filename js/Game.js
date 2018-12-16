aa7ae0d617c2762c45125fb845181f1e307786dc
verdantpath <m_dittmann@yahoo.com>
2018-12-16 12:31:25 -0800

    add token movement to event listener

--- js/Game.js
+++ js/Game.js
@@ -39,9 +39,9 @@ class Game {
   handleKeydown(e) {
     if(this.ready) {
       if(e.key === "ArrowLeft") {
-        // move left
+        this.activePlayer.activeToken.moveLeft();
       } else if (e.key === "ArrowRight") {
-        // move right
+        this.activePlayer.activeToken.moveRight(this.board.columns);
       } else if (e.key === "ArrowDown") {
         // play token
       }
