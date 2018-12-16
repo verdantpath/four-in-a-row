375896f6fe7b3787bf7836612fda756b4d4dad62
verdantpath <m_dittmann@yahoo.com>
2018-12-15 19:09:43 -0800

    add render method

--- js/Space.js
+++ js/Space.js
@@ -7,15 +7,17 @@ class Space {
     this.diameter = 76;
     this.radius = this.diameter / 2;
   }
-
+  /**
+   * Draws SVG space
+   */
   drawSVGSpace() {
-    constSVGSpace = document.createElementNS("http://www.w3.org/2000/svg", "circle");
-    svgSpace.setAttributesNS(null, "id", this.id);
-    svgspace.setAttributesNs(null, "cx", (this.x * this.diameter) + this.radius);
-    svgSpace.setAttributesNS(null, "cy", (this.y * this.diameter) + this.radius);
-    svgSpace.setAttributesNS(null, "r", this.radius - 8);
-    svgSpace.setAttributesNS(null, "fill", "black");
-    svgSpace.setAttributesNS(null, "stroke", "none");
+    const svgSpace = document.createElementNS("http://www.w3.org/2000/svg", "circle");
+    svgSpace.setAttributeNS(null, "id", this.id);
+    svgSpace.setAttributeNS(null, "cx", (this.x * this.diameter) + this.radius);
+    svgSpace.setAttributeNS(null, "cy", (this.y * this.diameter) + this.radius);
+    svgSpace.setAttributeNS(null, "r", this.radius - 8);
+    svgSpace.setAttributeNS(null, "fill", "black");
+    svgSpace.setAttributeNS(null, "stroke", "none");
     document.getElementById("mask").appendChild(svgSpace);
   }
 }
