73085326e827da377683812fff79ce1484a76a4f
verdantpath <m_dittmann@yahoo.com>
2018-12-16 10:56:08 -0800

    add event listener for handleKeydown method

--- js/app.js
+++ js/app.js
@@ -8,3 +8,10 @@ document.getElementById('begin-game').addEventListener('click', function() {
   this.style.display = 'none';
   document.getElementById('play-area').style.opacity = '1';
 });
+
+/**
+ * Listen for keyboard presses
+ */
+ document.addEventListener('keydown', function(event) {
+   game.handleKeydown(event);
+ });
