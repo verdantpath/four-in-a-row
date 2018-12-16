628794cf2dca0ea8f03686a37fb82f735dc85253
verdantpath <m_dittmann@yahoo.com>
2018-12-15 19:10:25 -0800

    add render method, add getter method

--- js/Token.js
+++ js/Token.js
@@ -4,4 +4,18 @@ class Token {
     this.id = `token-${index}-${owner.id}`;
     this.dropped = false;
   }
+  /**
+   * Draws new HTML token
+   */
+  drawHTMLToken() {
+    const token = document.createElement('div');
+    document.getElementById('game-board-underlay').appendChild(token);
+    token.setAttribute('id', this.id);
+    token.setAttribute('class', 'token');
+    token.style.backgroundColor = this.owner.color;
+  }
+
+  get htmlToken() {
+    return document.getElementById(this.id);
+  }
 }
