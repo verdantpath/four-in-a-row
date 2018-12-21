14030af12eaf4e539f45e2727ff3270a465d06fa
verdantpath <m_dittmann@yahoo.com>
2018-12-20 16:56:24 -0800

    add method to check if player has any more tokens

--- js/Player.js
+++ js/Player.js
@@ -34,4 +34,13 @@ class Player {
   get activeToken() {
     return this.unusedTokens[0];
   }
+
+  /**
+   * Check if a player has any undropped tokens left
+   * @return {Boolean}
+   */
+  checkTokens() {
+    return this.unusedTokens.length === 0 ? false : true;
+  }
+
 }
